0b0d9b7c4f44ed6c56693df6d1eb8c38c510f1fe
Duc Nguyen <ducnguyen201231@gmail.com>
2022-05-18 09:25:59 +0700

    chỉnh sửa otp

--- app/src/main/java/com/example/quanlynhapxuat/fragment/HomeFragment.java
+++ app/src/main/java/com/example/quanlynhapxuat/fragment/HomeFragment.java
@@ -21,9 +21,21 @@ import com.anychart.enums.HoverMode;
 import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
 import com.example.quanlynhapxuat.R;
+import com.example.quanlynhapxuat.api.ApiUtils;
+import com.example.quanlynhapxuat.model.DeliveryDocketDetail;
+import com.example.quanlynhapxuat.model.Product;
+import com.example.quanlynhapxuat.service.DeliveryDocketDetailService;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class HomeFragment extends Fragment {
@@ -48,38 +60,76 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         AnyChartView chartView=view.findViewById(R.id.chart_home);
         Cartesian cartesian = AnyChart.column();
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Rouge", 80540));
-        data.add(new ValueDataEntry("Foundation", 94190));
-        data.add(new ValueDataEntry("Mascara", 102610));
-        data.add(new ValueDataEntry("Lip gloss", 110430));
-        data.add(new ValueDataEntry("Lipstick", 128000));
+        ApiUtils.getProductService().getAllProduct().enqueue(new Callback<List<Product>>() {
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                List<Product> products = response.body();
+                DeliveryDocketDetailService.deliveryDocketService.getAllDeliveryDocketDetail().enqueue(new Callback<List<DeliveryDocketDetail>>() {
+                    @Override
+                    public void onResponse(Call<List<DeliveryDocketDetail>> call, Response<List<DeliveryDocketDetail>> response) {
+                        List<DeliveryDocketDetail> list = response.body();
+                        List<DataEntry> data = new ArrayList<>();
+                        Map<String, Integer> hashMap = new HashMap<>();
+                        int quantity = 0;
+                        for (Product product : products) {
+                            quantity = 0;
+                            for (DeliveryDocketDetail item : list) {
+                                if (product.getId() == item.getProductId()) {
+                                    quantity += item.getQuantity();
+                                }
+                            }
+                            hashMap.put(product.getName(), quantity);
+                        }
+                        List<Map.Entry<String, Integer>> ds = new ArrayList<Map.Entry<String, Integer>>();
+                        ds.addAll(hashMap.entrySet());
+
+                        Collections.sort(ds, new Comparator<Map.Entry<String, Integer>>() {
+                            @Override
+                            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                                return o1.getValue() > o2.getValue() ? -1 : 1;
+                            }
+                        });
+
+                        data.add(new ValueDataEntry(ds.get(0).getKey(), ds.get(0).getValue()));
+                        data.add(new ValueDataEntry(ds.get(1).getKey(), ds.get(1).getValue()));
+                        data.add(new ValueDataEntry(ds.get(2).getKey(), ds.get(2).getValue()));
+                        //data.add(new ValueDataEntry(ds.get(3).getKey(), ds.get(3).getValue()));
+                        //data.add(new ValueDataEntry(ds.get(4).getKey(), ds.get(4).getValue()));
+                        Column column = cartesian.column(data);
+
+                        column.tooltip()
+                                .titleFormat("{%X}")
+                                .position(Position.CENTER_BOTTOM)
+                                .anchor(Anchor.CENTER_BOTTOM)
+                                .offsetX(0d)
+                                .offsetY(5d)
+                                .format("${%Value}{groupsSeparator: }");
+                        cartesian.animation(true);
+                        cartesian.title("Thống kê 3 sản phẩm bán chạy nhất");
+                        cartesian.yScale().minimum(0d);
+                        cartesian.yAxis(0).labels().format("${%(Value)}");
+                        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+                        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+                        //        cartesian.xAxis(0).title("Product");
+                        //        cartesian.yAxis(0).title("Revenue");
+                        chartView.setChart(cartesian);
+                    }
+                    @Override
+                    public void onFailure(Call<List<DeliveryDocketDetail>> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+
+            }
+        });
 
 
-        Column column = cartesian.column(data);
-
-        column.tooltip()
-                .titleFormat("{%X}")
-                .position(Position.CENTER_BOTTOM)
-                .anchor(Anchor.CENTER_BOTTOM)
-                .offsetX(0d)
-                .offsetY(5d)
-                .format("${%Value}{groupsSeparator: }");
-
-        cartesian.animation(true);
-        cartesian.title("Thống kê 10 sản phẩm bán chạy nhất");
-
-        cartesian.yScale().minimum(0d);
-
-        cartesian.yAxis(0).labels().format("${%(Value)}");
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-        cartesian.interactivity().hoverMode(HoverMode.BY_X);
 
 //        cartesian.xAxis(0).title("Product");
-//        cartesian.yAxis(0).title("Revenue");
-
-
-        chartView.setChart(cartesian);
+//        cartesian.yAxis(0).title("Revenue"
     }
 }
--- app/src/main/java/com/example/quanlynhapxuat/fragment/MoreFragment.java
+++ app/src/main/java/com/example/quanlynhapxuat/fragment/MoreFragment.java
@@ -2,46 +2,53 @@ package com.example.quanlynhapxuat.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.quanlynhapxuat.R;
 import com.example.quanlynhapxuat.activity.EmployeesActivity;
 import com.example.quanlynhapxuat.activity.KhachHang.ListKHActivity;
+import com.example.quanlynhapxuat.activity.main.LoginActivity;
 import com.example.quanlynhapxuat.activity.main.MainActivity;
 
 public class MoreFragment extends Fragment {
 
     Button btn_nv, btn_kh,  btn_dx;
+    TextView tvName;
+    ImageView ivAvatar;
+    MainActivity mainActivity;
+    private View mView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
-
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        setControl(view);
+        mView=inflater.inflate(R.layout.fragment_more, container, false);
+        mainActivity= (MainActivity) getActivity();
+            btn_nv = mView.findViewById(R.id.btn_nv);
+            btn_kh = mView.findViewById(R.id.btn_kh);
+            tvName=mView.findViewById(R.id.tv_nameNV);
+            btn_dx = mView.findViewById(R.id.btn_dx);
+            ivAvatar = mView.findViewById(R.id.ivAvatar);
+            Log.e("more", LoginActivity.nameLogin );
+            tvName.setText(LoginActivity.nameLogin);
+            if (LoginActivity.linkAvatar!=null) {
+                Glide.with(mainActivity).load(LoginActivity.linkAvatar).into(ivAvatar);
+                // Inflate the layout for this fragment
+            }
         setEvent();
-    }
+        return mView;
 
-    private void setControl(View view) {
-        btn_nv = view.findViewById(R.id.btn_nv);
-        btn_kh = view.findViewById(R.id.btn_kh);
-
-        btn_dx = view.findViewById(R.id.btn_dx);
 
     }
 
